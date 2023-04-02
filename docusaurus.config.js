3c587a5c5f4ef8bfa43891325118f7f3d17afc41
Dominic Ruggiero <dominic@userexe.me>
2023-04-01 23:52:30 -0400

    ok so this i know how to fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,10 +10,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://querycord.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
